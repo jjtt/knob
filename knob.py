a0a368fd12ac12df7362ab8fdc65dc75e35e094f
juho <jjtt@mbnet.fi>
2016-02-12 16:46:35 +0200

    test reading input devices

--- knob.py
+++ knob.py
@@ -8,11 +8,19 @@ def main(argv=None):
   if argv is None:
     argv = sys.argv
 
-  if len(argv) != 1:
+  if len(argv) != 2:
     print("input device missing")
+    devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
+    for device in devices:
+      print(device.fn, device.name, device.phys)
     return
 
-  print("todo")
+  devicePath = argv[1]
+
+  device = evdev.InputDevice(devicePath)
+
+  for event in device.read_loop():
+    print(evdev.categorize(event))
 
 
 
